555e47e9caca0763d53d66b2667522317c863f42
Davyd <polishchuk.david.v@gmail.com>
2024-05-15 20:51:55 +0300

    Upgrade (home work 3.1)

--- Lesson_2/Homework_3.1.py
+++ Lesson_2/Homework_3.1.py
@@ -1,14 +1,6 @@
 y = [1,1,9]
 
 if len(y)>1:
-    last_element = y.pop()
-    y.insert(0, last_element)
-    print(y)
-else:
-    print()
+    y.insert(0, y.pop())
 
-if len(y) == 1:
-    print(y)
-
-if len(y) == 0:
-    print(y)
+print(y)
