1adc351c09f3472002a265bfde560a7ef449cdae
Davyd <polishchuk.david.v@gmail.com>
2024-06-04 02:53:39 +0300

    Ready (home work 4.1)

---
+++ Lesson_3/Homework_4.1.py
@@ -0,0 +1,14 @@
+lis1 = [3, 4, 5, 5, 6, 0, 8, 0, 11, 142, 0, 0, 12]
+
+lis2 = []
+
+while True:
+  if 0 in lis1:
+     lis1.remove(0)
+     lis2.append(0)
+  else:
+      break
+
+lis3 = lis1+lis2
+
+print(lis3)
