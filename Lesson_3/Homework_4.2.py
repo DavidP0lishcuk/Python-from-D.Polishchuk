ccd65f63b1238530357935dafc22027e2cf27317
Davyd <polishchuk.david.v@gmail.com>
2024-06-17 18:48:13 +0300

    Update (home work 4.2)

--- Lesson_3/Homework_4.2.py
+++ Lesson_3/Homework_4.2.py
@@ -1,9 +1,15 @@
-list1 = [1, 2, 3, 4, 5, 6]
+list1 = [0, 1, 7, 2, 4, 8]
 
 sum1 = 0
 
-for index in range(0,len(list1),1):
-    if index % 2 == 0:
-        sum1 = sum1+list1[index]
+if not list1:
+    print(0)
 
-print(list1[-1]*sum1)
+else:
+    for index in range(0, len(list1), 1):
+        if index % 2 == 0:
+            sum1 = sum1+list1[index]
+
+if list1:
+    print(list1[-1] * sum1)
+
