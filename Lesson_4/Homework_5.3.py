2a1a7a58e0fa059dac577612baf47d0660805424
Davyd <polishchuk.david.v@gmail.com>
2024-06-17 19:49:15 +0300

    Update (home work 5.3)

--- Lesson_4/Homework_5.3.py
+++ Lesson_4/Homework_5.3.py
@@ -1,15 +1,25 @@
 
-punctuation = r"""!"#$%&' ()*+,-./:;<=>?@[\]^_`{|}~"""
+punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+space = " "
 
 user_case = input("Some word:")
-
-user_case = user_case.title()
 
 for s1 in punctuation:
     if s1 in user_case:
         user_case = user_case.replace(s1, '')
 
+user_case = user_case.title()
+
+for s2 in space:
+    if s2 in user_case:
+        user_case = user_case.replace(s2, '')
+
 if len(user_case) > 139:
     user_case = user_case[:139]
 
 print("#" + user_case)
+
+#Python Commnity
+#i like python community!
+#Should, I. subscribe? Yes!
+#t!e@S#T e$XA%m
