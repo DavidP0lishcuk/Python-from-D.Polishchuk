26e8033b8793df269bee2ee89d8069e51097d883
Davyd <polishchuk.david.v@gmail.com>
2024-06-17 20:38:02 +0300

    Update (home work 4.1)

--- Lesson_3/Homework_4.1.py
+++ Lesson_3/Homework_4.1.py
@@ -1,14 +1,10 @@
 lis1 = [3, 4, 5, 5, 6, 0, 8, 0, 11, 142, 0, 0, 12]
 
-lis2 = []
+l2 = 0
 
-while True:
-  if 0 in lis1:
+for l2 in lis1:
+    if l2 == 0:
      lis1.remove(0)
-     lis2.append(0)
-  else:
-      break
-
-lis1 = lis1+lis2
+     lis1.append(0)
 
 print(lis1)
